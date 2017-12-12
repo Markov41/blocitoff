f01c4bc11aa3ddc86b8433485c5d6f64e52760f4
Kevin Lattner <analogy48@yahoo.com>
2017-12-12 19:35:29 +0000

    Add Faker to seed database

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'devise'
 
+gem 'faker'
+
 gem 'bootstrap-sass'
 
 # Use jquery as the JavaScript library
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+ 
+ 15.times do
+  Item.create!(
+   
+   name: Faker::Lovecraft.sentence,
+   created_at: Faker::Date.backward(5)
+   )
+ end
+ item = Item.all
+ 
+  10.times do
+  User.create!(
+   
+   email: Faker::Internet.email,
+   password: Faker::Internet.password
+   )
+ end
+ user = User.all
+ 
+ 
+ 
+ puts "Faker Seeds Finished"
+ puts "#{Item.count} items created with Faker"
+ puts "#{User.count} users created with Faker"
